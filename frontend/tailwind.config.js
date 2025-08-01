b414434056549d39697aeee79f5696d0d090e467
Jonathan Kwan <jonathankwan056@gmail.com>
2025-08-01 23:27:01 +0800

    fix: update profile UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,13 +7,13 @@ export default {
   theme: {
     extend: {
       colors: {
-         'primary': '#000000',       // Merah Terang (untuk header, tombol utama, footer)
-        'secondary': '#B6B09F',      // Merah Tua (untuk hover, judul penting)
-        'accent': '#000000',         // Aksen untuk link hover dan elemen aktif
-        'background': '#FFFDEF',    // Putih Gading (latar belakang utama halaman)
-        'neutral': '#F1F1F1',        // Abu-abu Muda (latar card, pemisah seksi)
-        'text-main': '#000000',     // Teks Utama (hitam pekat)
-        'text-secondary': '#555555', // Teks Sekunder (abu-abu muda),
+         'primary': '#000000',
+        'secondary': '#B6B09F',
+        'accent': '#000000',
+        'background': '#FFFDEF',
+        'neutral': '#F1F1F1',
+        'text-main': '#000000',
+        'text-secondary': '#555555',
       },
       fontFamily: {
         'sans': ['Inter', 'ui-sans-serif', 'system-ui'],
